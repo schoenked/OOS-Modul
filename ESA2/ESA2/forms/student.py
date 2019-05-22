0957e3111c5a099df4bb57a9b0c8be1935b4fc75
schoenked <33178062+schoenked@users.noreply.github.com>
2019-05-23 00:20:50 +0200

    backup

--- ESA2/ESA2/forms/student.py
+++ ESA2/ESA2/forms/student.py
@@ -1,10 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 from ESA2.models import Student
+from ESA2.models import Lehrveranstaltung
 
 class StudentForm(ModelForm):
     class Meta:
         model = Student
         fields = "__all__" 
+        
+        lvs = forms.ModelMultipleChoiceField(queryset=Lehrveranstaltung.objects.filter())
+
         labels = {
             'forename' : 'Vorname',
             'lastname' : 'Nachname',
--- ESA2/ESA2/models.py
+++ ESA2/ESA2/models.py
@@ -3,9 +3,18 @@ from django.db import models
 class Student(models.Model):
     forename=models.CharField(max_length=50)
     lastname=models.CharField(max_length=50)
+    lehrveranstaltungen = models.ManyToManyField('Lehrveranstaltung')
+
+    def __str__(self):
+        return "{} {}".format(self.lastname, self.forename)
 
 class Lehrveranstaltung(models.Model):
     bezeichnung=models.CharField(max_length=50)
+    teilnehmer = models.ManyToManyField(Student, through=Student.lehrveranstaltungen.through)
+    
+    def __str__(self):
+        return self.bezeichnung
+    
 
 
 
