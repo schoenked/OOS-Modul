facf8c883172305915bf86fd3dda1fd18124c3ec
schoenked <33178062+schoenked@users.noreply.github.com>
2019-05-27 12:47:28 +0200

    so bleibt es

--- ESA2/ESA2/models.py
+++ ESA2/ESA2/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
 class Student(models.Model):
-    forename=models.CharField(max_length=50)
-    lastname=models.CharField(max_length=50)
-    lehrveranstaltungen = models.ManyToManyField('Lehrveranstaltung')
+    forename = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
 
     def __str__(self):
         return "{} {}".format(self.lastname, self.forename)
 
 class Lehrveranstaltung(models.Model):
-    bezeichnung=models.CharField(max_length=50)
-    teilnehmer = models.ManyToManyField(Student, through=Student.lehrveranstaltungen.through)
-    
+    bezeichnung = models.CharField(max_length=50)
+    teilnehmer = models.ManyToManyField(Student, blank=True)
+
     def __str__(self):
         return self.bezeichnung
     
